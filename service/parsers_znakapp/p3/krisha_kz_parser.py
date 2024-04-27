c742c568e7d8ec7e4b4c62b67eb327c791ee4504
Daniil <dan.akunets@mail.ru>
2024-04-28 00:41:16 +0300

    new krisha parser

--- service/parsers_znakapp/p3/krisha_kz_parser.py
+++ service/parsers_znakapp/p3/krisha_kz_parser.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
 import time
 from PIL import Image
@@ -12,28 +14,29 @@ base_dir = '/home/djangoapp/znakapp/service'
 python_path = sys.executable
 mask_path = os.path.join(base_dir,'lama','3','prepare_masks.py')
 mask_id_path = os.path.join(base_dir,'lama','3','prepare_masks_id.py')
-static_out_dir = os.path.join(base_dir, 'parsers_znakapp','p3', 'images')
+static_out_dir =  os.path.join(base_dir, 'parsers_znakapp','p3', 'images')
 txt_out_dir = os.path.join(base_dir, 'parsers_znakapp','p3')
-ua = UserAgent()
 options = Options()
-# desktop_user_agent = ua.random
 
-# while "Mobile" in desktop_user_agent or "Tablet" in desktop_user_agent:
-#     desktop_user_agent = ua.random
-
-# Используем полученный агент
 options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+options.add_argument("--headless")  # Запуск Chrome в режиме без головы
+options.add_argument("--no-sandbox")  # Отключение режима песочницы для работы в изолированном окружении
+options.add_argument("--disable-dev-shm-usage")  # Отключение использования общей памяти для предотвращения проблем с производительностью
+options.add_argument("--disable-gpu")  # Отключение аппаратного ускорения, полезно для режима без головы
+options.add_argument("--window-size=1920,1080")  # Задание размера окна
+options.add_argument("----enable-automation")
+options.add_argument("--disable-extensions")
 
-driver = webdriver.Chrome(options=options)
-driver.set_window_size(1920, 1080)
-
+try:
+    driver = webdriver.Chrome(options=options)
+except Exception as ex:
+    print(f'{ex}')
 def parsing_krisha_kz(link):
     try:
         driver.get(link)
         response = requests.get(link).text
         soup = BeautifulSoup(response,'lxml')
-        time.sleep(2)
-        close_modal = driver.find_element(By.XPATH,'/html/body/main/div[2]/div/div[1]/div/div/div[1]/div/button')# Закрывает модалку
+        close_modal = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/main/div[2]/div/div[1]/div/div/div[1]/div/button')))
         close_modal.click()
         try:
             check_owner = soup.find('div',class_='owners__name owners__name--large').text #Проверка на владельца (Хозяин недвижимости)
@@ -43,16 +46,16 @@ def parsing_krisha_kz(link):
         imgs_src_list = soup.find_all('div', class_='gallery__small-item')
         count_of_click = len(imgs_src_list) - 1 # кол-во кликов в модальном окне, где у нас фотка
         print(count_of_click)
-        first_img_to_click = driver.find_element(By.CLASS_NAME,'gallery__small-item')
-        time.sleep(1)
+        first_img_to_click = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'gallery__small-item')))
         first_img_to_click.click() # Открытие модального окна
         time.sleep(1)
         i = 0
         download_links = [] # список, куда будут заноситься ссылки наши на скачивание изображения
         while i <= count_of_click:
-            image_link = driver.find_element(By.XPATH,"/html/body/div[6]/div/div/div/div/img")
+            image_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[6]/div/div/div/div/img")))
             download_links.append(image_link.get_attribute('src'))
-            next_img = driver.find_element(By.XPATH,'/html/body/div[6]/div/div/div/a[2]')
+            #next_img = driver.find_element(By.XPATH,'/html/body/div[6]/div/div/div/a[2]')
+            next_img = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[6]/div/div/div/a[2]")))
             next_img.click()
             time.sleep(0.5)
             i+=1
@@ -79,21 +82,22 @@ def parsing_krisha_kz(link):
                     new_width = 900
                     resize_image = image.resize((new_height,new_width),Image.BILINEAR)
                     resize_image.save(os.path.join(directory,f"new_{filename}"))
-                    
+
         offer_info = soup.find('div', class_='offer__advert-title').find('h1').text.strip()
         print(offer_info)
 
         with open(f'{txt_out_dir}/info_list.txt', 'w', encoding='utf-8') as file:
-            file.write(f'{offer_info}')  
-            
+            file.write(f'{offer_info}')
+
         # Получаем список файлов в директории
         files = os.listdir(directory)
 
         # Удаляем файлы, соответствующие заданному шаблону
         for file in files:
             if file.startswith('img') and file.endswith('.png'):
-                os.remove(os.path.join(directory, file))           
+                os.remove(os.path.join(directory, file))
         # удаляем фотки, и выбираем потом нужную маску
+        
         if check_owner==0:
             subprocess.call([python_path, mask_path])
         else:
